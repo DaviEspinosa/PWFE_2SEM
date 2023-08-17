2ec0a759b07ac17e96386933a8928eddddf96dbd
DaviEspinosa <123770867+DaviEspinosa@users.noreply.github.com>
2023-08-17 16:52:39 -0300

    Delete ProjetoGrupo directory

--- ProjetoGrupo/login.js
+++
@@ -1,6 +0,0 @@
-function mostrarCadastro() {
-    document.getElementById("loginForm").style.display = "none"; //chamando o meu form do HTML
-    
-    document.getElementById("cadastroForm").style.display = "block";
-}
-
