34f9582b3412286ffebec6dd931dbd4eb4f18a86
DaviEspinosa <123770867+DaviEspinosa@users.noreply.github.com>
2023-08-17 13:13:21 -0300

    Add files via upload

---
+++ ProjetoGrupo/login.js
@@ -0,0 +1,6 @@
+function mostrarCadastro() {
+    document.getElementById("loginForm").style.display = "none"; //chamando o meu form do HTML
+    
+    document.getElementById("cadastroForm").style.display = "block";
+}
+
