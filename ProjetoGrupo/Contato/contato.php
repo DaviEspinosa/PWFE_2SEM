26bc6dcf34d3a51d7e1a7301b20e049e81b0e443
DaviEspinosa <123770867+DaviEspinosa@users.noreply.github.com>
2023-08-17 23:50:55 -0300

    Add files via upload

---
+++ ProjetoGrupo/Contato/contato.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="contato.css">
+</head>
+<body>
+<header>
+      <nav>
+        <a class="logo" href="">&lt;/games&gt;</a>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+          <li><a href="">Home</a></li>
+          <li><a href="">Produtos</a></li>        
+          <li><a href="">Contato</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="main">
+        <div class="contato">
+            <div class="left">
+            <div>
+                <h3>Contato</h3>
+            </div>
+            <div>
+                <p>TELEFONE: (19) 4002-8922</p>
+            </div>
+            <div>
+                <p>E-MAIL: games@gmail.com</p>
+            </div>
+            <div>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+            </div>
+        </div>
+        <hr>
+            <div class="right">
+                <input class="nameInput" type="name" placeholder="Nome*">
+                <input class="emailInput" type="email" placeholder="E-mail*">
+                <textarea name="message" id="" cols="30" rows="10">Mensagem</textarea>
+                <button>Enviar</button>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="contato.js"></script>
+<script src="https://kit.fontawesome.com/26d4a64054.js" crossorigin="anonymous"></script>
+</html>
